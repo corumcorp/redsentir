43a062d8ffa1c41c7fa0667ad281e12162da8e8b
zariguella <bermudezdiegoalejandro@gmail.com>
2017-08-30 12:29:35 -0500

    Foros

--- redsentir/foro/models.py
+++ redsentir/foro/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Foro(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    introduccion = models.CharField(max_length=20)
+    tema = models.CharField(max_length=200)
+    imagen = models.ImageField(upload_to='static/images/foros/', default='static/images/foros/defaultUser.png')
+    
+    def __str__(self):
+        return self.tema
+
+class Comentario(models.Model):
+    foro = models.ForeignKey(Foro, on_delete=models.CASCADE)
+    mensaje = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.foro.tema+" | "+self.mensaje
--- redsentir/redsentir/settings.py
+++ redsentir/redsentir/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*',]
 
 INSTALLED_APPS = [
     'seguridad.apps.SeguridadConfig',
+    'foro.apps.ForoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
