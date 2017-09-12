6b0bd8b1ec68542a9732e5e7a4a34783bab811ce
zariguella <bermudezdiegoalejandro@gmail.com>
2017-09-12 13:37:09 -0500

    foro

--- redsentir/foro/views.py
+++ redsentir/foro/views.py
@@ -6,12 +6,12 @@ def index(request):
     if 'mensaje' in request.POST :
         comentario = Comentario(mensaje=request.POST['mensaje'],foro_id=1)
         comentario.save()
-    comentarios = Comentario.objects.all
+    comentarios = Comentario.objects.all().reverse()
     return render(request, 'sitio/foro/index.html',{'comentarios':comentarios})
 
 @login_required
 def comentar(request):
     comentario = Comentario(mensaje=request.POST['mensaje'],foro_id=1)
     comentario.save()
-    comentarios = Comentario.objects.all
+    comentarios = Comentario.objects.all().reverse()
     return render(request, 'sitio/foro/index.html',{'comentarios':comentarios,})
