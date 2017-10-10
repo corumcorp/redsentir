4d05bba27b8ee2aa6d2e2e009c3035159d108496
zariguella <bermudezdiegoalejandro@gmail.com>
2017-10-09 21:13:05 -0500

    comentarios

--- redsentir/foro/views.py
+++ redsentir/foro/views.py
@@ -4,18 +4,11 @@ from .models import Comentario
 @login_required
 def index(request):
     if 'mensaje' in request.POST :
-        comentario = Comentario(mensaje=request.POST['mensaje'],foro_id=1)
+        comentario = None
+        if 'anonimo' in request.POST :
+            comentario = Comentario(mensaje=request.POST['mensaje'],foro_id=1)
+        else :
+            comentario = Comentario(usuario=request.user,mensaje=request.POST['mensaje'],foro_id=1)
         comentario.save()
     comentarios = Comentario.objects.all().order_by('id').reverse()
     return render(request, 'sitio/foro/index.html',{'comentarios':comentarios})
-
-@login_required
-def comentar(request):
-    comentario = None
-    if 'anonimo' in request.POST:
-        comentario = Comentario(mensaje=request.POST['mensaje'],foro_id=1)
-        request.POST['anonimo']
-    else :
-        comentario = Comentario(usuario=request.user,mensaje=request.POST['mensaje'],foro_id=1)
-    comentario.save()
-    redirect('index')
--- redsentir/redsentir/settings.py
+++ redsentir/redsentir/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
         'HOST':'localhost',
         'PORT': '',
     }
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 
 
