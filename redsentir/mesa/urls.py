eea292686c3313e5b1721f022f518e7cf322d158
zariguella <bermudezdiegoalejandro@gmail.com>
2018-02-05 17:40:33 -0500

    mapa mesa regional

--- redsentir/mesa/urls.py
+++ redsentir/mesa/urls.py
@@ -3,4 +3,10 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', index, name='inicio'),
+    url(r'^marinilla$', marinilla, name='marinilla'),
+    url(r'^abejorral$', abejorral, name='abejorral'),
+    url(r'^sanluis$', sanluis, name='sanluis'),
+    url(r'^sanfrancisco$', sanfrancisco, name='sanfrancisco'),
+    url(r'^argelia$', argelia, name='argelia'),
+    url(r'^sonson$', sonson, name='sonson'),
 ]
--- redsentir/mesa/views.py
+++ redsentir/mesa/views.py
@@ -3,5 +3,31 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'sitio/mesa/mapa.html')
+
+@login_required
+def marinilla(request):
+    return render(request, 'sitio/mesa/marinilla.html')
+
+@login_required
+def argelia(request):
+    return render(request, 'sitio/mesa/argelia.html')
+
+@login_required
+def sonson(request):
+    return render(request, 'sitio/mesa/sonson.html')
+
+@login_required
+def abejorral(request):
+    return render(request, 'sitio/mesa/abejorral.html')
+
+@login_required
+def sanluis(request):
+    return render(request, 'sitio/mesa/sanluis.html')
+
+@login_required
+def sanfrancisco(request):
+    return render(request, 'sitio/mesa/sanfrancisco.html')
+
+
     
