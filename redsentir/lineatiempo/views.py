1c6f286cd0bc18978ac3218b09d40e290c8f88f8
John Anderson <gomezmunera@gmail.com>
2018-02-05 18:22:27 -0500

    guias docentes, linea de tiempo

--- redsentir/lineatiempo/views.py
+++ redsentir/lineatiempo/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def inicio(request):
     return render(request, 'sitio/lineatiempo/inicio.html')
