a4e1b166f45c794f1bbb0bca1b6dfe02b0aa985f
zariguella <bermudezdiegoalejandro@gmail.com>
2018-03-22 10:04:40 -0500

    usuario repetido

--- redsentir/seguridad/views.py
+++ redsentir/seguridad/views.py
@@ -15,24 +15,24 @@ def registro(request):
             usuarioTmp = User.objects.filter(username = request.POST['username']).first()
             if usuarioTmp != None:
                 return render(request, 'registration/registro.html',{'usuario':'el nombre de usuario '+request.POST['username']+' ya existe'})            
-                if 'password' in request.POST and 'password1' in request.POST and request.POST['password'] != '':
-                    if request.POST['password'] == request.POST['password1'] :                    
-                        usuario = User.objects.create_user(username = request.POST['username'],password = request.POST['password'],email = request.POST['email'])
-                        perfil = None
-                        if 'genero' in request.POST :
-                            pgenero = request.POST['genero']
-                        else :
-                            pgenero = None
-                        if not 'avatar' in request.FILES :
-                            perfil = Perfil(user_id=usuario.id,genero=pgenero)
-                        else :
-                            perfil = Perfil(user_id=usuario.id,avatar=request.FILES['avatar'],genero=pgenero)                   
-                        perfil.save()
-                        return HttpResponseRedirect(reverse(login,args=[]))
+            if 'password' in request.POST and 'password1' in request.POST and request.POST['password'] != '':
+                if request.POST['password'] == request.POST['password1'] :                    
+                    usuario = User.objects.create_user(username = request.POST['username'],password = request.POST['password'],email = request.POST['email'])
+                    perfil = None
+                    if 'genero' in request.POST :
+                        pgenero = request.POST['genero']
                     else :
-                        return render(request, 'registration/registro.html',{'password':'las claves deben coincidir'})
+                        pgenero = None
+                    if not 'avatar' in request.FILES :
+                        perfil = Perfil(user_id=usuario.id,genero=pgenero)
+                    else :
+                        perfil = Perfil(user_id=usuario.id,avatar=request.FILES['avatar'],genero=pgenero)                   
+                    perfil.save()
+                    return HttpResponseRedirect(reverse(login,args=[]))
                 else :
-                    return render(request, 'registration/registro.html',{'password':'ingrese clave de seguridad valida'})
+                    return render(request, 'registration/registro.html',{'password':'las claves deben coincidir'})
+            else :
+                return render(request, 'registration/registro.html',{'password':'ingrese clave de seguridad valida'})
         else :
             return render(request, 'registration/registro.html',{'usuario':'debe ingresar un usuario'})            
     else :
