dc3ad26b4cad6034b3ee478d277244edd20c440c
John Anderson <gomezmunera@gmail.com>
2018-04-25 10:42:59 -0500

    expertos foro identificacion

--- redsentir/foro/templatetags/grupo.py
+++ redsentir/foro/templatetags/grupo.py
@@ -6,6 +6,6 @@ register = template.Library()
 #@register.simple_tag
 @register.filter(name='esExperto')
 def esExperto(user, nombre_grupo):
-    #grupo = Group.objects.get(name=nombre_grupo)
-    #return True if grupo in user.groups.all() else False
-    return user.groups.filter(name=nombre_grupo).exists()
+    grupo = Group.objects.get(name=nombre_grupo)
+    return True if grupo in user.groups.all() else False
+    #return user.groups.filter(name=nombre_grupo).exists()
