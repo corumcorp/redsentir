bf70c11b33a3d59384daf89ad3bb5e03aef0b3fa
John Anderson <gomezmunera@gmail.com>
2018-08-25 14:46:32 -0500

    version beta del juego nuevo

---
+++ redsentir/static/Beta1/src/settings.8e8f8.js
@@ -0,0 +1,263 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "687mFi0WlLhahoE49fhVxP": [
+                "Music/S_Splash.mp3",
+                0
+            ],
+            "4ewjqtT0NFq6QQtc/3auVA": [
+                "Music/music_logo.mp3",
+                0
+            ],
+            "a0yly1lS1DxYvPbAwyUj3O": [
+                "Texture/Esper_Simple/esperma_sencilla-0.png",
+                1
+            ],
+            "7abDDKm8hHyIU3s5NPSllv": [
+                "Texture/EspermGome.png",
+                1
+            ],
+            "0fXecI+YZGTZ4sqLSHJLJk": [
+                "Texture/Explosion/explosion-0.png",
+                1
+            ],
+            "039SvdI1NGrqthNNelVbPh": [
+                "Texture/GameOv.png",
+                1
+            ],
+            "547NLRVn5B4JIE2h9Dffrg": [
+                "Texture/Inicio.png",
+                1
+            ],
+            "8dezjH9x1KWKMix1IoJtKG": [
+                "Texture/arbolesPEQ.png",
+                1
+            ],
+            "3emLTzcwFN/o02LRdJvvWH": [
+                "Texture/casasPEQ.png",
+                1
+            ],
+            "2bCS+NcaJNcZGTsl18wlOg": [
+                "Texture/castillo3.png",
+                1
+            ],
+            "2bayTSXfJAGYjE7oeJKBdo": [
+                "Texture/cielo2.png",
+                1
+            ],
+            "1fKLET2+FPFqOjLr+Akx14": [
+                "Texture/fondo_verdeP.png",
+                1
+            ],
+            "bfkKirSvpIBJRo4liuEYD1": [
+                "Texture/monta_fondoP.png",
+                1
+            ],
+            "e0++1Ydr1K4oG9RuUzOVkQ": [
+                "Texture/rocasPEQ.png",
+                1
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                1
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                1
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                1
+            ],
+            "d8HsitJHxOYqo801xBk8ev": [
+                "image/default_panel.png",
+                1
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.AudioClip",
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/Scene/JUEGO_INTER.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/JUEGO_INTER.fire",
+            uuid: 5
+        },
+        {
+            url: "db://assets/Scene/Inicio.fire",
+            uuid: 0
+        },
+        {
+            url: "db://assets/Scene/GameOver.fire",
+            uuid: 1
+        }
+    ],
+    packedAssets: {
+        "0431c7331": [
+            2,
+            0,
+            "91an4XRBBCfomQdCpFV4TT",
+            3,
+            4
+        ],
+        "0c00f8b3": [
+            1,
+            2,
+            "c4zy6yF9FMGIOgAUjde5TQ",
+            3,
+            4
+        ],
+        "0e527d618": [
+            "00M0aQXaBNraVh9C/3fIbz",
+            "05fW/BWEpJtpdKtjGvH0r9",
+            "09xqLUAFFLSo6TBBWn0SRF",
+            "0aJuQEP8dN6bAUnrZ+eB/J",
+            "0dZXma3eVCyrnxm84mFzE6",
+            "15QftoT11PT5+wzFsGH0/0",
+            "16xfDWunhPN4D05M+TKCt3",
+            "18BtKSGg5G9Ia95FIcR1hi",
+            "1cf5PQuHJKV7APMI8UdC6u",
+            "1e5zhrtExLwIv+M2d7qQ4u",
+            "23Vp7u2xVLRKDMnbyatcGD",
+            "24a5uyq/hOVrssQOhN9R9d",
+            "27NiOITk9Bc67izOPO5kzk",
+            "28WuS9fHRNlYPEdqdJSJjZ",
+            "2aPuQvd/pOI76i/BDtBbJF",
+            "2dbnLsfgtJY61LHSv7Qjq2",
+            "2ejfxAh9JJnYwmmNZvLmrm",
+            "30T5p94ilPNqH84Om8w9lI",
+            "31El+f4ltN+rPsdk7mwCVR",
+            "31XPmYvQpCl4kJaLYEjVkK",
+            "32NFOb7WhL3o3iRNd1oZhi",
+            "33Y6J1XSNEYZG0jwgwGwlL",
+            "334ZBmWCpFQalH0Hr/gk02",
+            "3dstX/HN1DSYp9JfHaxIAv",
+            "3e4L1Vrn9LqqUVRhjGNNBb",
+            "40iKvY/J9CVKFHSoicC4V9",
+            "41TeModStPJ5MBLQVmm0pa",
+            "41f1z33YhOJ5wfJFlvdUNX",
+            "42XCw0xbVChL3zRfUFgVE2",
+            "42bL8GmUVOP411tcguDeh8",
+            "42s8LM78JJkpQF+rKLrBFv",
+            "43AiETQ+tJqYL7yxvoL0xv",
+            "49SiFLlOhOeovMTffouE5j",
+            "4ct/AQNSBAOJ/5YGgC/qWN",
+            "4d0hGY7X5HqJD8953axfUK",
+            "4eJkX/2l1Aj4A1VYsVjqis",
+            "52eY/opSlLKoYisq1TbUoT",
+            "53FAiIQgRGqJyBAMdW8Nak",
+            "56KbT4GQNOHJ7Gg94M2DYQ",
+            "58eRFHt6BDjbWJuWAecE6G",
+            "5e7ydZQP1GtZJkM/dNQ/+6",
+            "5fNWrTL8VAw60+dfvmapSm",
+            "61Jfu6gm1DipbDvhpLIuuI",
+            "63cnlpUHJHjp5yRCrs1T4w",
+            "68BZ7To15LgbdJ+ibsSRy6",
+            "68LLYxOF1OYoNg9gzCojg4",
+            "6bulPoBGZANrsNPDg1Z70B",
+            "6dTFG7JEtHuoDYz6z8zUgh",
+            "74hM6nsZhISI9wYrlQDJSR",
+            "75WTcIMrBC+K9i4Gduqf3W",
+            "79/U75M/5K57Vj+jWDc482",
+            "81cHerkIxPYr/CiupbM7rT",
+            "81imE9LJdP273QIF0RNcjV",
+            "81nsBg1K9FpIt+sDHl0mMC",
+            "84INdZzalE0anq8/WTgTv1",
+            "87Epmp8mJG0bADEcghp4Dp",
+            "88+Lhei3NN3bkCqnFJYniN",
+            "8dITrfeGNEFKvYQ1B+GWIb",
+            "90PVDKAnlA3ZPmwBxPCD2V",
+            "91GR8rpUpPDZ0aH7y26ses",
+            "93XaFx0wBJ04i0qzhnlo4/",
+            "93rpAFTwdArIFlvZiJ0ymk",
+            "94XyjAfmlBc6cD0F1yMPxZ",
+            "94lckKuP1PjJuuooZLjuLt",
+            "949zaYClxLU5nQvX0z7pqH",
+            "9745+ovylL76j+TRnvgWmM",
+            "9bvaMerUlDyary99mJa6xp",
+            "9ftlEiOXRAWZ7ezsPnpXa4",
+            "9f9fwtu1tMqJlm10hs1L4+",
+            "a0o2TCu3FJJb3eLCM7Hf5H",
+            "a51u+sfJ9DrZht9/ajbiiP",
+            5,
+            "a8kVtC/7FNvZxYgbaoffCN",
+            "acc0dVac9BYor4w5fvmM7/",
+            "aeNpz6VgxJrbcQAhHwFL4A",
+            "aeiRAuWE5KqLGQzW/d9+MS",
+            "ae65U0NYhPgqQ/nMAhtNkw",
+            "b1voFMYs5CP5+/kuGtugKp",
+            "b2QbNR4n1KM7pvPiQPjTS6",
+            "b5Gld1P09HwJFNgiBW1q/C",
+            "bd3X5dZqlIhpZqE/veQlRX",
+            "beXwZJpclNVbTzWDKtyLiP",
+            "be6dCBlQpA34SUzbA+9eyd",
+            "c3nLO3hTtK9Y47U9fYBNT1",
+            "c8XjYvPOFNyq7zObW5ZtZ6",
+            "ccMXXKqf9Fkos1EBUxuqQ/",
+            "ccuOmYAm9MfKmZqOqR+f6J",
+            "ce3B+8hRNM97MAxU8QW9Il",
+            "cfiL2gnd1B96+EeL1VikK/",
+            "d5secYO7RJkqOAx4utERH1",
+            "d7vVeNjTNOKIWb0iYosCRi",
+            "d8TmQLMdhIs5fR6Te5a2It",
+            "dcSyXksy1HQaXwrOdHrHf3",
+            "dd0u26mvpHX7zquHIwabX5",
+            "e0JsuyUYRI0resiE0bVZhH",
+            "e0VWBexq1JYZPdibZHwlP7",
+            "e0krLpai5Gb70Ar8FoTIwa",
+            "e1l4HXGdJFLbZjGptMeeRf",
+            "e2J00nav5IXo0whqZQ9YCd",
+            "eenpF7vIRF5YQZyj+tWIjD",
+            "f3C49JLRZEIph6OKrEQWve",
+            "f6y5l7+ylEebR4f4hz3Fqm",
+            "f8hl9rQl1L97IjJ9Ni3eiH",
+            "faAGrylO5HNKvjw4m0hGIM",
+            "fdqW4/Jd1G57I6nu8D7lwu"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "77AZXXFRdNQ7WCwQ+9Rsy2",
+        "23b52lFx9Ad6GMnOI6tgIX",
+        "29FYIk+N1GYaeWH/q1NxQO",
+        "e97GVMl6JHh5Ml5qEDdSGa",
+        "f0BIwQ8D5Ml7nTNQbh1YlS",
+        "a7Sj+xAm5PkJ/exNE4IN/2"
+    ],
+    "md5AssetsMap": {
+        "04/0431c7331.json": "624ab",
+        "0c/0c00f8b3.json": "81f88",
+        "0e/0e527d618.json": "8098e",
+        "assets/Music/music_logo.mp3": "d580e",
+        "assets/Music/S_Splash.mp3": "3f4d8",
+        "assets/Texture/arbolesPEQ.png": "d7e64",
+        "assets/Texture/casasPEQ.png": "0b77c",
+        "assets/Texture/castillo3.png": "b433f",
+        "assets/Texture/cielo2.png": "86ac8",
+        "assets/Texture/Esper_Simple/esperma_sencilla-0.png": "ad1bc",
+        "assets/Texture/EspermGome.png": "997f8",
+        "assets/Texture/Explosion/explosion-0.png": "5bf6e",
+        "assets/Texture/fondo_verdeP.png": "3c9ba",
+        "assets/Texture/GameOv.png": "3e330",
+        "assets/Texture/Inicio.png": "d238c",
+        "assets/Texture/monta_fondoP.png": "a871a",
+        "assets/Texture/rocasPEQ.png": "77b1b",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/image/default_panel.png": "cdbc9"
+    }
+};
